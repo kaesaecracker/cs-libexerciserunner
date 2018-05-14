5b1153d4715453ca6b71d108bf9f3b559e9eeae4
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-05-14 16:52:08 +0200

    rename

---
+++ Runner.Reflection.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace LibConsoleRunner
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using Library;
+
+    public partial class Program
+    {
+        private static List<ExerciseWithInfo> GetExercises()
+        {
+            string executingDll = Assembly.GetExecutingAssembly().Location;
+            string ownPath = Path.GetDirectoryName(executingDll);
+            var dllFiles = (
+                from d in Directory.EnumerateFiles(ownPath)
+                where d.EndsWith("dll")
+                      && d != executingDll
+                select d
+            ).ToArray();
+
+            Log.Debug("Own path: {p}", ownPath);
+            Log.Debug("DLL files to load: {@dll}", dllFiles.Select(Path.GetFileName));
+
+            Log.Information("Loading DLL files in current directory");
+            foreach (string dll in dllFiles)
+            {
+                try
+                {
+                    Assembly.LoadFile(dll);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is FileLoadException || ex is BadImageFormatException)
+                    {
+                        Log.Warning("Could not load a dll file: {@exMsg}", ex.Message.Trim());
+                        Log.Debug("{@ex}", ex);
+                    }
+                    else throw;
+                }
+            }
+
+            var loadedAssemblys = (
+                from a in AppDomain.CurrentDomain.GetAssemblies()
+                where !a.FullName.StartsWith("System")
+                      && !a.FullName.StartsWith("Serilog")
+                select a
+            ).ToList();
+            Log.Debug("Assemblys loaded: {@a}", loadedAssemblys.Select(a => a.GetName().Name));
+
+            return (
+                from a in loadedAssemblys
+                from t in a.GetTypes()
+                where t.IsClass
+                where t.IsDefined(typeof(ExerciseAttribute), true)
+                select new ExerciseWithInfo(
+                    t,
+                    t.GetCustomAttribute(typeof(ExerciseAttribute)) as ExerciseAttribute)
+            ).ToList();
+        }
+
+        private static List<TaskWithInfo> GetTasks(ExerciseWithInfo exercise)
+            => GetTasks(new List<ExerciseWithInfo> {exercise});
+
+        private static List<TaskWithInfo> GetTasks(List<ExerciseWithInfo> exercises)
+        {
+            var methods = (
+                from e in exercises
+                from m in e.Exercise.GetMethods()
+                select m
+            ).ToList();
+            Log.Debug("Availiable methods: {@m}", methods);
+
+            var tasks = (
+                from m in methods
+                where m.IsDefined(typeof(TaskAttribute), false)
+                select m
+            ).ToList();
+            Log.Debug("Availiable tasks: {@t}", tasks);
+
+            return (
+                from m in tasks
+                let twi = new TaskWithInfo(
+                    m,
+                    m.GetCustomAttribute(typeof(TaskAttribute)) as TaskAttribute
+                )
+                orderby twi.Number
+                select twi
+            ).ToList();
+        }
+    }
+}
---
+++ Runner.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Library;
+
+namespace LibConsoleRunner
+{
+    using Serilog;
+    using Serilog.Core;
+
+    public partial class Program
+    {
+        private static readonly Logger Log = new LoggerConfiguration()
+                                             .MinimumLevel.Verbose()
+                                             .WriteTo.Console(
+#if !DEBUG
+                                                LogEventLevel.Warning
+#endif
+                                             ).WriteTo.File(
+                                                 path: "logs/runner.log"
+                                             ).CreateLogger();
+
+        private static List<ExerciseWithInfo> _exercises = null;
+
+        static void Main(string[] args)
+        {
+            _exercises = GetExercises();
+            Log.Debug("Exercises: {@exercises}", _exercises);
+
+            Console.Write("AlgoExercises Runner\n" +
+                          "Whenever you are prompted for a decision after a list you can type the" +
+                          " text in front of the colon (':')"); // TODO or the beginning of the name
+
+            switch (args.Length)
+            {
+                case 1:
+                    TaskAssistant(args[0]);
+                    break;
+                case 2:
+                    TaskAssistant(args[0], args[1]);
+                    break;
+
+                default:
+                    if (args.Length != 0)
+                        Console.WriteLine("Allowed numbers of parameters: [0, 1, 2]\n" +
+                                          $"Number of parameters specified: {args.Length}\n" +
+                                          "Ignoring console parameters.");
+                    TaskAssistant();
+                    break;
+            }
+        }
+
+        private static void TaskAssistant(string exerciseNumber)
+        {
+        }
+
+        private static void TaskAssistant(string exerciseNumber, string taskNumber)
+        {
+        }
+
+        private static void TaskAssistant()
+        {
+            Console.WriteLine("The following tasks are availiable:");
+            foreach (var exercise in _exercises)
+            {
+                Console.WriteLine($"#{exercise.Number}: {exercise.Name}");
+            }
+        }
+
+        private struct ExerciseWithInfo
+        {
+            public Type Exercise { get; }
+
+            public string Number => this._attribute.Number;
+            public string Name => this._attribute.Name;
+            public string Description => this._attribute.Description;
+
+            private readonly ExerciseAttribute _attribute;
+
+            public ExerciseWithInfo(Type exercise, ExerciseAttribute attribute)
+            {
+                this.Exercise = exercise;
+                this._attribute = attribute;
+            }
+        }
+
+        private struct TaskWithInfo
+        {
+            public MethodInfo Task { get; }
+
+            public string Number => this._attribute.Number;
+            public string Name => this._attribute.Name;
+            public string Description => this._attribute.Description;
+
+            private readonly TaskAttribute _attribute;
+
+            public TaskWithInfo(MethodInfo task, TaskAttribute attribute)
+            {
+                this.Task = task;
+                this._attribute = attribute;
+            }
+        }
+    }
+}
